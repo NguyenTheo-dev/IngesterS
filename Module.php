f058d142cdbfb128b0fca73d17a8be5b75c37f67
NguyenTheo-dev <nguyen_t@univ-corse.fr>
2023-02-27 11:02:18 +0100

    corrected module.php

--- Module.php
+++ Module.php
@@ -6,4 +6,11 @@ use Omeka\Module\AbstractModule;
 class Module extends AbstractModule
 {
 
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    
+
 }
